e79cd97a2c87ab0676d9cbedabbe1d10518b8929
Kristian-Roopnarine <48737383+Kristian-Roopnarine@users.noreply.github.com>
2019-07-11 19:43:51 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 //global variable to hold
-const timerStart = 1500;
-var time= 1500;
-var sessions;
-var currentSession = 1;
-var wasOnBreak = false;
-var isWorking = true;
-var initialTime;
-var started = false;
 
+/*
 var timeData = (function(){
 
   var working = function(){
@@ -103,18 +96,21 @@ var controller = (function(timeCtrl,UICtrl){
 
   //restarts timer on click
   if(started){
-    restart.addEventListener('click',backToTimer);
-    var d = setInterval(timeCtrl.countDown,1000);
-    var c = setInterval(UICtrl.changeTime,1000);
-    clearInterval(d);
-    clearInterval(c);
-    started=false;
+    restart.addEventListener('click',function(){
+      var d = setInterval(timeCtrl.countDown,1000);
+      var c = setInterval(UICtrl.changeTime,1000);
+      clearInterval(d);
+      clearInterval(c);
+      started=false;
+      backToTimer;
+    });
+
   }
 
 
   //pauses on click
   if(started){
-    pause.addEventListener('click',countdown)
+    pause.addEventListener('click',freeze)
     started=false;
   }
   /*pause.addEventListener('click',function(){
@@ -125,7 +121,7 @@ var controller = (function(timeCtrl,UICtrl){
     clearInterval(d);
     clearInterval(c);
   });*/
-
+/*
   //sets custom time
   customTime.addEventListener('click',function(){
     time = parseInt(timeInput.value) * 60;
@@ -140,3 +136,115 @@ var controller = (function(timeCtrl,UICtrl){
   })
 
 })(timeData,UIController);
+*/
+
+const timerStart = 1500;
+var time= 1500;
+var sessions = 3;
+var sessionInput;
+var initialTime;
+var currentSession = 0;
+
+var start = document.getElementById("start");
+var pause = document.getElementById("pause");
+var restart = document.getElementById("restart");
+var sessionDisplay = document.getElementById("sessionDisplay");
+var sessionBtn = document.getElementById("sessions");
+var sessionInput = document.getElementById("sessionInput");
+var clock = document.getElementById("clock");
+var timer = document.getElementById("timer");
+var timeBtn = document.getElementById("time");
+var timeInput = document.getElementById("timeInput");
+var d;
+var c;
+var onBreak = false;
+var initialTime = 0;
+var breakAudio = new Audio('rs-intro.mp3');
+var workAudio = new Audio('work-audio.mp3');
+
+//countdown
+function countDown(){
+  if(time > 0)
+    time--;
+    console.log(time);
+    if (time == 0){
+      if (currentSession == sessions){
+        time = 1800;
+        currentSession = 0;
+        onBreak = true;
+        breakAudio.play();
+        setTimeout(function(){breakAudio.pause()},10000);
+      } else if (onBreak){
+        currentSession++;
+        time = initialTime;
+        onBreak = false;
+        workAudio.play();
+        setTimeout(function(){workAudio.pause()},10000);
+      } else if (!onBreak){
+        time = 65;
+        onBreak = true;
+        breakAudio.play();
+        setTimeout(function(){breakAudio.pause()},10000);
+      }
+    }
+}
+
+//updates UI display for time and sessions
+function changeTimeDisplay(){
+  var timeMinutes = Math.floor(time/60); //
+  if (timeMinutes === 0){
+    timeMinutes = '00';
+  } else if (timeMinutes < 10){
+    timeMinutes = '0' + timeMinutes;
+  }
+
+  var timeSeconds = time % 60;
+  //if divisible by 60
+  if (timeSeconds === 0 ){
+    timeSeconds = '00';
+  } else if (timeSeconds < 10){
+    timeSeconds = '0'+ timeSeconds;
+  }
+  var newTime = `${timeMinutes}:${timeSeconds}`; //displays time as (Minutes:Seconds)
+  clock.innerHTML = newTime;
+  sessionDisplay.innerHTML = `Sessions: ${currentSession}/${sessions}`;
+}
+
+//get input values
+
+timeBtn.addEventListener('click',function(){
+    if (parseInt(timeInput.value) > 0){
+      time = parseInt(timeInput.value) * 60;
+      initialTime = parseInt(timeInput.value) * 60;
+      changeTimeDisplay();
+    }
+});
+
+
+
+sessionBtn.addEventListener('click',function(){
+    if (parseInt(sessionInput.value) > 0){
+      sessions = parseInt(sessionInput.value);
+      changeTimeDisplay();
+    }
+});
+
+
+//start on click
+start.addEventListener('click', function(){
+  currentSession++;
+  var d = setInterval(countDown,1000);
+  var c = setInterval(changeTimeDisplay,1000);
+  //pause if clicked
+  pause.addEventListener('click', function(){
+    clearInterval(d);
+    clearInterval(c);
+  });
+  //restart on click
+  restart.addEventListener('click', function(){
+    time = timerStart;
+    clearInterval(d);
+    clearInterval(c);
+    changeTimeDisplay();
+  });
+});
